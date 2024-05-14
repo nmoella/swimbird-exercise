1a87a5d7734d97c0cb1f872c1b0392e03744a6af
Nico Meller <nico.meller@mailbox.org>
2024-05-14 16:01:04 +0200

    add material, theme, basic navbar and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgForOf} from "@angular/common";
+import {routes} from "./app.routes";
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, MatButtonModule, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'exercise';
+  protected readonly routes = routes;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import {Page1Component} from "./components/page1/page1.component";
+import {Page2Component} from "./components/page2/page2.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'page1',
+    title: 'Page 1',
+    component: Page1Component
+  },
+  {
+    path: 'page2',
+    title: 'Page 2',
+    component: Page2Component,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'page1',
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'page1'
+  }
+];
