32a48f595979e353e0dfdc19d0dcf1b676d0e012
Nico Meller <nico.meller@mailbox.org>
2024-05-16 15:09:36 +0200

    display accounts as table

--- src/app/routes/table/table.component.ts
+++ src/app/routes/table/table.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {AccountService} from "../../services/account.service";
 import {NgIf} from "@angular/common";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {Account} from "../../services/account";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef, MatHeaderRow,
+  MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from "@angular/material/table";
+import {CdkTableDataSourceInput} from "@angular/cdk/table";
 
 @Component({
   selector: 'app-page2',
@@ -11,24 +21,34 @@ import {Account} from "../../services/account";
   imports: [
     MatButton,
     NgIf,
-    MatProgressSpinner
+    MatProgressSpinner,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatHeaderRow,
+    MatRow
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
   protected loading: boolean = false;
+  dataSource: CdkTableDataSourceInput<Account> = [];
+  displayedColumns: string[] = ['id', 'accountId', 'bank', 'balance', 'currency'];
 
   constructor(
     private accountService: AccountService
   ) {}
 
-  public async loadAccounts(): Promise<Account[]> {
+  public async loadAccounts(): Promise<void> {
     this.loading = true;
-    let accounts: Account[] = await this.accountService.getAccounts();
+    this.dataSource = await this.accountService.getAccounts();
     this.loading = false;
-
-    return accounts;
   }
 
   public isLoaded(): boolean {
